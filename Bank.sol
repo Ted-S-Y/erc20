a45199e960cdf36cabc96d0ac50921b634a39bbd
Ted-S-Y <51975052+Ted-S-Y@users.noreply.github.com>
2022-11-10 10:43:54 +0900

    Update Bank.sol

--- Bank.sol
+++ Bank.sol
@@ -1,44 +1,39 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-contract Token {
-    string public name = "Mytoken";
-    string public symbol = "MTN";
-    uint8 public decimals = 18;
-    uint256 public totalSupply;
+contract Bank {
+    string  public BankName="MyBank";
+    uint balance;
+    address public owner;
 
-    mapping( address => uint256) _balances;
+    event Deposit (address from, uint256 value);
+    event Withdraw(address to, uint256 value);
 
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-    
-    constructor() public {
-        totalSupply = 10000 * 10**decimals;
-        _balances[msg.sender] = totalSupply;
-    }
-    
-    function balanceOf(address _owner) public view returns (uint256 balance) {
-        return _balances[_owner];
-    }
-    
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        require( _value <= _balances[msg.sender]);
-        _balances[msg.sender]  -=  _value;
-        _balances[_to]         +=  _value;
-        return true;
+    constructor() payable{
+        balance=msg.value;
+        owner = msg.sender;
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-         require( _value <= _balances[_from]);
-				 _balances[_from]  -=  _value;
-         _balances[_to]    +=  _value;
-         return true;
-    }
-    
-    function approve(address _spender, uint256 _value) public returns (bool success) {
-    
-    }
-    
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining){
+    modifier onlyOwner(){
+        require( msg.sender == owner,"msg.sender is not owner");
+        _;
     }
 
-    
+    function deposit() public payable onlyOwner {
+        balance += msg.value;
+        emit Deposit(msg.sender, msg.value); 
+    }
+
+    function withdraw(uint256 value) public payable onlyOwner {
+        balance -=value;
+        payable(msg.sender).transfer(value);
+        emit Withdraw(msg.sender, value);
+    }
+
+    function getBalance() public view returns(uint256){
+        return balance;
+    }
+
+    fallback () external payable{
+        deposit();
+    }
 }
