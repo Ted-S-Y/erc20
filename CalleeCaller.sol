95356ba4318f2892f926049f940800c3f0d19f81
Ted-S-Y <51975052+Ted-S-Y@users.noreply.github.com>
2022-11-11 10:08:27 +0900

    Create CalleeCaller.sol

---
+++ CalleeCaller.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.8.0;
+
+
+contract Callee {
+    address public caller;
+    
+    event Called(address indexed);
+    
+    constructor() public {
+    }
+    
+    function func() public returns (bool result){
+        caller = msg.sender;
+        emit Called(caller);
+        result = true;
+    }
+}
+
+contract Caller {
+    Callee public _callee;
+    constructor(address _ca) public {
+        _callee = Callee(_ca);
+    }
+    function funcDefaultCall() public returns (bool result){
+        result = _callee.func();
+    }
+    function funcStaticCall() public returns (bool){
+        (bool result,) = address(_callee).call(abi.encodePacked(bytes4(keccak256("func()"))));
+        return result;
+    }
+    function funcDelegateCall() public returns (bool ){
+        (bool result,) = address(_callee).delegatecall(abi.encodePacked(bytes4(keccak256("func()"))));
+        return result;
+    }   
+}
